0ed6a9901bc55c037a2c571256da2b14a88a63b4
Samariddin <zuhurovsamariddinn@gmail.com>
2024-09-07 20:43:10 +0300

    lorem

--- app/(root)/upgrade/page.jsx
+++ app/(root)/upgrade/page.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
 
 function Upgrade() {
   return (
     <div>
       Upgrade
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, culpa.
+        Sint soluta suscipit, aut ipsam deleniti in reprehenderit cupiditate id
+        provident nihil numquam fugiat quidem dicta quasi dolorem, maiores, eos
+        sapiente. Ipsa exercitationem ipsam ut. Vero libero ut error cupiditate
+        nihil! Laborum voluptatem amet aliquam accusantium itaque soluta ab
+        animi sapiente, deleniti vero modi consectetur explicabo est, illo
+        atque, ipsum reprehenderit eveniet veniam similique ipsa commodi? Nihil
+        molestiae quam cupiditate. A optio consequuntur voluptate. Quis quisquam
+        fugiat eius accusantium ipsam, sequi maxime odio. Voluptatem mollitia
+        explicabo neque officiis aspernatur aut beatae repellat consequatur
+        libero architecto cumque magnam hic, tenetur optio nihil quae commodi
+        perferendis illo, velit voluptatum itaque dolor? Iste praesentium nulla
+        eos quo magnam iure perspiciatis debitis illo non ducimus eius in, sed
+        quis unde eum corporis sint fugit inventore, facere illum. Ex
+        praesentium, sequi accusantium animi, nobis suscipit illum doloribus
+        repudiandae fugit adipisci laboriosam consequuntur nesciunt. Eaque
+        repellendus unde ipsam totam. Cupiditate corrupti tempore facilis sunt
+        temporibus facere numquam iusto quibusdam voluptatem architecto
+        voluptatibus inventore laudantium, animi fugiat dolorum dolore, minus
+        nemo similique, molestiae asperiores rerum commodi? Soluta dolore iste
+        vero veritatis ut pariatur, impedit rerum sit assumenda obcaecati esse
+        tenetur temporibus cum earum consectetur eius. Maxime itaque nihil
+        deserunt voluptates voluptas! Autem possimus asperiores eaque voluptatem
+        dolorum? Possimus accusamus sequi, tempora magnam velit ipsam nostrum
+        exercitationem aperiam repellat blanditiis neque id excepturi,
+        laboriosam animi ducimus recusandae fugit dignissimos, ab voluptatibus
+        dolor amet? Pariatur ipsa reprehenderit corporis illum qui optio culpa
+        recusandae incidunt quas fuga mollitia nam quia sequi soluta saepe,
+        dolorem iusto. Provident totam, deserunt voluptatum obcaecati temporibus
+        eaque sit explicabo veniam similique, distinctio illo doloremque alias
+        ab tempora magnam consequuntur expedita iusto rem officia repellendus
+        saepe quibusdam maiores. Veniam perferendis commodi ea eius ducimus
+        provident nihil hic pariatur quasi quae, omnis, repudiandae dolore
+        repellendus. Architecto.
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Upgrade
+export default Upgrade;
